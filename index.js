e78fb9f5dd1624fb56b8b6b37f5c74b8370a909d
oyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-07-16 14:39:49 +0100

    Dom documented

---
+++ index.js
@@ -0,0 +1,18 @@
+// grt the DOM
+
+const quizContainer = document.querySelector(".quiz-container");
+const quizHeader = document.querySelector(".quiz-header");
+const questionEl = document.getElementById("question");
+const quiz = document.getElementById("quiz");
+const answerEl = document.getElementsByClassName("answer");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const btn_submit = document.getElementById("submit");
+
+// set Questions
+
+const quizData{
+    
+}
